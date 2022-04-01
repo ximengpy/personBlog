f1fd6132fea2d85c35a4ade89bcfa3b76f216ca4
pengyan <1580559335@qq.com>
2022-04-01 12:08:16 +0800

    修改上线路径

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,8 @@
 function moduleConfig() {
   const env = import.meta.env.MODE || 'prod'
   const api = {
-    'development': 'http://localhost:80/api'
+    'development': 'http://localhost:80/api',
+    'production': '/api'
   }
   return {
     env,
